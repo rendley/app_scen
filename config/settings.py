9e4d72c96f66af96bdf20b65896b6b70499b3006
ivan_grigorev <rauscral@gmail.com>
2021-05-02 04:45:00 +0300

    add nginx and all work. Nginx give static file

--- config/settings.py
+++ config/settings.py
@@ -129,7 +129,7 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static_cdn'
+STATIC_ROOT = BASE_DIR / 'static_dev'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
