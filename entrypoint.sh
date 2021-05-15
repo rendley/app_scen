a854ee0924bfd160d19a5365c477ecc38f96b3f2
ivan_grigorev <rauscral@gmail.com>
2021-05-15 04:02:21 +0300

    version 0.5: add volumes posgresql and create first dump

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,10 @@
 python manage.py makemigrations --no-input
 python manage.py migrate --no-input
 python manage.py collectstatic --no-input
-
+#python manage.py createsuperuser
+#admin
+#admin@example.com
+#12345
 
 # For local
 #python manage.py runserver 0.0.0.0:5000
