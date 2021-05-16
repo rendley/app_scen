2074cace812f0fe343a1a3cf7559befcf21bfcf0
ivan_grigorev <rauscral@gmail.com>
2021-05-16 22:25:33 +0300

    version 0.6 first deploy

--- config/settings_dev.py
+++ config/settings_dev.py
@@ -18,14 +18,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-e@$qi51k=n*qs&3msavy^-idlco#m(03bhg7)id5njrp=3x7p8'
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = 'django-insecure-e@$qi51k=n*qs&3msavy^-idlco#m(03bhg7)id5njrp=3x7p8'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-DEBUG = int(os.environ.get("DEBUG", default=0))
+DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -77,12 +77,8 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'prod_db',
-        'USER': 'admin',
-        'PASSWORD': 'devpass',
-        'HOST': 'postgresdb',
-        'PORT': 5432,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
